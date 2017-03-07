a067fda91b56f5e9e5080c943ede450c6328b6c9
gonghaima@hotmail.com <5418854188>
2017-03-08 01:18:35 +1300

    yoman react webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
+
+import {Hello} from './app/hello';
+
+import './index.css';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Hello}/>
+  </Router>,
+  document.getElementById('root')
+);
